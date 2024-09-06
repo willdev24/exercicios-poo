4094f7e90b6159c208d38f22b020810a39fdb814
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-09-05 21:43:25 -0400

    pk

---
+++ exercicio04.js
@@ -0,0 +1,64 @@
+/* 
+criar class conta bancaria abstrata  e e criar duas class prototicas de conta bancaria, 1- constaorrente // 2= conta poupança  */
+
+
+class contaBancaria{
+
+constructor(nome,numeCONta,doc,valor){
+
+    if(this.constructor == contaBancaria){
+        throw new Error("essa classe nao pede ser instancada");
+        
+    }
+
+    this.nome = nome
+    this.numeCONta = numeCONta
+    this.doc = doc
+    this.valor = valor
+}
+
+ get sacar(){
+
+
+}
+
+set sacar(_saque){
+
+}
+
+deposito(_deposito){
+
+}
+
+extrato(){
+
+    if(this.constructor == contaCorrente ){
+        throw new Error("ultilizar o polimorfismo");
+        
+    }
+    }
+    
+}
+
+
+
+
+class contaCorrente extends contaBancaria{
+
+constructor(nome,numeCONta,doc,valor){
+super(nome, numeCONta, doc, valor)
+
+
+}
+
+extrato(){
+
+}
+
+
+
+
+const teste = new contaCorrente("willy")
+
+teste.extrato()
+
